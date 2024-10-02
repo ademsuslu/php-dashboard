90fa2b82a43b93a63812ef633255b08a3f6f1bf9
ademsuslu <ademsuslu9080@gmail.com>
2024-10-02 16:08:53 +0300

    skeleton created

---
+++ data/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2024-2025 
+      <a href="https://my-portfolio-three-eta-33.vercel.app/" target="_blank">Suslu</a>.</strong>
+      All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.0.5
+    </div>
+  </footer>
